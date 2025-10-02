8171bfb3fb704eef68e2a68333ccada094c47b44
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-10-01 22:14:38 -0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,10 +19,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        
         applicationId = "com.example.historias_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +37,39 @@ android {
 
 flutter {
     source = "../.."
+}
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")  
+    id("kotlin-android")
+}
+
+android {
+    namespace "com.example.historias_app"  
+    compileSdk 34
+
+    defaultConfig {
+        applicationId "com.example.historias_app"
+        minSdk 21
+        targetSdk 34
+        versionCode flutterVersionCode.toInteger()
+        versionName flutterVersionName
+    }
+}
+
+dependencies {
+
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    implementation("com.google.firebase:firebase-auth")
+
+
+    implementation("com.google.firebase:firebase-firestore")
+
+
+    implementation("com.google.firebase:firebase-storage")
 }
