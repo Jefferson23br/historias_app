fab74e27931b95bbf3a93322fe465418d4f32643
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-10-01 22:14:31 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,8 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+}
