1e877eca5c35ae7c85b77949609b4b8cab411ed3
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-10-11 23:08:54 -0300

    Update routes.dart

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../pages/login_page.dart';s
+import '../pages/login_page.dart';
 import '../pages/home_page.dart';
 import '../pages/story_page.dart';
 import '../pages/premium_page.dart';
